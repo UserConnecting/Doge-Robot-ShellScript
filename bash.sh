83d9bb5e4f69c17175870b2ff57ba1810fde848f
UserConnecting <97047842+UserConnecting@users.noreply.github.com>
2022-10-29 02:43:36 -0300

    Add files via upload

---
+++ bash.sh
@@ -0,0 +1,158 @@
+#!/bin/bash
+
+echo -e "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⢄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+echo -e "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⢳⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⢖⡚⢆⠀⠀⠀⠀⠀⠀⠀⠀"
+echo -e "⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡏⠀⠀⢳⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡠⠞⠁⠀⠀⡜⡇⠀⠀⠀⠀⠀⠀⠀"
+echo -e "⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⠀⠀⠀⠤⠷⠒⠦⠤⠤⠤⢤⣀⡠⠊⢀⠁⡀⠀⠀⠀⡇⠀⠀⠀⠀⠀  "
+echo -e "⠀⠀⠀⠀⠀⠀⠀⠀⣀⠷⠚⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁⠀⡀⠀⠐⡐⠋⠀⡄⡇⠀⠀⠀⠀⠀⠀⠀"
+echo -e "⠀⠀⠀⠀⠀⢀⡴⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢧⡀⠔⠁⠀⡐⢰⠇⠀⠀⠀⠀⠀⠀⠀"
+echo -e "⠀⠀⠀⠀⣰⠋⠀⠀⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠓⢤⡀⠀⡜⢦⠀⠀⠀⠀⠀⠀⠀"
+echo -e "⠀⠀⠀⢠⡇⠀⠀⢈⣾⣿⡆⠀⠀⠀⠀⢀⡠⣤⣄⠀⠀⠀⠀⠀⠀⠀⠀⠃⠠⡇⠘⡆⠀⠀⠀⠀⠀⠀"
+echo -e "⠀⠀⠀⡞⠀⠀⠀⠙⣻⠟⠁⠀⠀⠀⠀⢾⣜⣿⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁⠀⢧⠀⠀⠀⠀⠀⠀"
+echo -e "⠀⠀⢠⠃⠀⢀⣤⣾⣥⣄⠀⠀⠀⠀⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢼⠀⠀⠀⠀⠀⠀"
+echo -e "⠀⠀⡞⠀⢰⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢧⠀⠀⠀⠀⠀"
+echo -e "⠀⠀⡇⠀⢸⢈⠻⡿⠿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⡀⠀⠀⠀⠀"
+echo -e "⠀⠀⢳⠀⠀⠱⣤⣧⣀⡀⠀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢧⠀⠀⠀⠀"
+echo -e "⠀⠀⠈⡆⠀⠀⠹⡉⠙⠉⠛⠛⠺⠋⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⡆⠀⠀⠀"
+echo -e "⠀⠀⠀⢣⡀⠀⠀⠙⠒⠒⠒⠒⠒⠒⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠀⠀⠀"
+echo -e "⠀⠀⠀⠀⢳⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+echo -e "⠀⠀⠀⢀⠞⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+echo -e "⠀⠀⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+
+start_case(){
+  case $INP in
+
+        *escan*|*scan*|*vuln*|*nmap*|*portsc*|*portas*)
+
+                echo -e "➤ Digite o IP para ser escaneado: " && read -p "↳ " HOST
+                echo $HOST | egrep '(\b25[0-5]|\b2[0-4][0-9]|\b[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}' > /dev/null && echo -e 'Conectando ao IP' && OK="1" || echo -e 'Impossível conectar a esse IP'
+
+                if [  $OK == "1"  ]; then
+                    nmap $HOST -T5 -oX $HOST_result.xml && sudo apt install xsltproc && xsltproc $HOST_result.xml -o $HOST-SCAN.html && rm *SCAN.xml
+
+                    HOSTScan=("Scan em $IPHOST foi finalizado." "O escaneamento de portas em $IPHOST foi concluído!" "$IPHOST terminou de ser escaneado." "O scan do Nmap no $IPHOST foi finalizado.")
+
+                    echo -e "➤ ${HOSTScan[$((RANDOM%4))]}"
+
+                else
+                    echo -e "➤ Host Inválido."
+
+                fi
+
+                Ask
+                ;;
+
+        *namor*|*fican*|*casad*|*compromet*)
+
+                Namora=('Estou bem solteiro.' 'Que tipo de robô namora?' 'Eu não gosto dessa ideia de namorar.' 'Não tenho pretendentes.')
+                echo -e "➤ ${Namora[$((RANDOM%4))]}"
+
+                Ask
+        ;;
+
+        *hor*|*manh*|*tard*|*noit*)
+
+                HORA=`date +%H`
+
+                if [ $HORA -ge 01 ] && [ $HORA -lt 12 ]; then
+                        echo -e "↳ Horário ➤ `date +%H:%M` da manhã!"
+
+                elif [ $HORA -ge 12 ] && [ $HORA -lt 18 ]; then
+                        echo -e "↳ Horário ➤ `date +%H:%M` da tarde!"
+
+                else
+                        echo -e "↳ Horário ➤ `date +%H:%M` da noite!"
+
+                fi
+
+                Ask
+        ;;
+
+        *criador*|*desenvolv*|*criou*|*criado*)
+
+                Owner=('Fui feito pelo Joe RM93410.' 'Meu criador é o Joe RM93410.' 'Joe RM93410 foi quem me criou.' 'O Joe RM93410 me desenvolveu.')
+                echo -e "➤ ${Owner[$((RANDOM%4))]}"
+
+                Ask
+        ;;
+
+        *vers*|*updat*|*atuali*|*upgrad*)
+
+                Atual=('Estou verificando os repositórios..' 'Procurando atualizações..' 'Verificando se precisa de atualizações..' 'Buscando por atualizações..')
+                echo -e "➤ ${Atual[$((RANDOM%4))]}"
+
+                sudo apt update 2>&1 > /dev/null && sudo apt upgrade -y 2>&1 /dev/null
+
+                sleep 2
+
+                Att_fin=('O sistema está atualizado!' 'Todos os pacotes estão atualizados.' 'Todas as aplicações estão atualizadas.' 'Todos os aplicativos se encontram na última versão.')
+                echo -e "➤ ${Att_fin[$((RANDOM%4))]}"
+
+                Ask
+        ;;
+
+        *servi*)
+
+                Services=('Estou procurando por serviços ativos..' 'Localizando serviços ativos..' 'Estou procurando quais serviços estão ativos..' 'Vou verificar os serviços ativos..')
+                echo -e "➤ ${Services[$((RANDOM%4))]}"
+
+                sleep 2
+
+                ss -atun
+
+                Ask
+        ;;
+
+        *proce*|*taref*)
+
+                Process=('Veja os processos ativos abaixo.' 'Aqui estão os processos:' 'Esses são os processos ativos na sua máquina:' 'Estes processos estão ativos:')
+                echo -e "➤ ${Process[$((RANDOM%4))]}"
+
+                ps
+
+                Ask
+        ;;
+
+        *apache*)
+                Apache=('Arquivos no diretório /var/www/html:' 'Arquivos no root directory do Apache:' 'Seus arquivos do Apache:' 'Estes são os arquivos do seu Apache:')
+                echo -e "➤ ${Apache[$((RANDOM%4))]}"
+
+                ls /var/www/html
+
+                Ask
+        ;;
+
+        *nano*|*edit*)
+
+                Nano=('Abrindo editor NANO..' 'Aguarde, executando editor NANO..' 'Executando o melhor editor..' 'Editor de texto NANO será executado..')
+                echo -e "➤ ${Nano[$((RANDOM%4))]}"
+
+                sleep 2
+
+                nano
+
+                Ask
+        ;;
+
+        *)
+
+                Nothing=('Não consigo te ajudar com isso, me desculpe.' 'Não entendi o que você quer, prometo que vou melhorar' 'Perdão, não consegui entender você.' 'Procurarei evoluir para te entender.')
+                echo -e "➤ ${Nothing[$((RANDOM%4))]}"
+
+                Ask
+        ;;
+
+esac
+
+}
+
+Ask(){
+  echo
+  echo -e "Diga ao doge o que precisa."
+  read -p "↳ " INP
+  start_case
+}
+
+echo
+
+Ask
